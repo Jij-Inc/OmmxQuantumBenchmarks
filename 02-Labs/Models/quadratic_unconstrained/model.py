6130471a027011be3df7f42b4e0fdd9c3512d761
hao <w11179627@gmail.com>
2025-08-06 20:52:49 +0900

    Add quadratic_unconstrained version model

---
+++ 02-Labs/Models/quadratic_unconstrained/model.py
@@ -0,0 +1,36 @@
+import jijmodeling as jm
+import ommx
+import numpy as np
+
+def create_problem():
+    # Define sets
+    I_set = jm.Placeholder("I", ndim=1)  
+    K_set = jm.Placeholder("K", ndim=1)  
+    n = I_set.len_at(0, latex="n")
+
+    # Define parameters
+    P = jm.Placeholder("P", ndim=0) 
+
+    # Define decision variables
+    x = jm.BinaryVar("x", shape=I_set.shape, description="Binary variable x")
+    z = jm.BinaryVar("z", shape=(I_set.shape[0], K_set.shape[0]), description="Binary variable z")
+
+    # Define elements
+    k = jm.Element("k", belong_to=(0,n-1))
+    i = jm.Element("i", belong_to=(0,n-k-1))
+
+    # Define the problem
+    problem = jm.Problem("Energy_Minimization", sense=jm.ProblemSense.MINIMIZE)
+
+    # Define the objective function
+    first_term = jm.sum(k, 
+        (jm.sum(i, 4 * z[i, k] - 2 * x[i] - 2 * x[i + k + 1] + 1)) ** 2
+    )
+
+    second_term = P * jm.sum(k,
+        jm.sum(i, 3 * z[i, k] - 2 * z[i, k] * x[i] 
+            - 2 * z[i, k] * x[i + k + 1] + x[i] * x[i + k+1])
+    )
+
+    problem += first_term + second_term
+    return problem
