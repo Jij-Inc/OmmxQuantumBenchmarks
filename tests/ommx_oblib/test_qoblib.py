6271ea69991c51320426fa01b37bfa57c6ea8b26
ksk-jij <keisuke.sato@j-ij.com>
2025-08-27 11:52:48 +0900

    #23 Design more test cases

--- tests/ommx_oblib/test_qoblib.py
+++ tests/ommx_oblib/test_qoblib.py
@@ -86,3 +86,180 @@ def test_marketsplit():
     5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
     6. the evaluated solution with its instance and solution is the same as the original solution.
     """
+    # 1. its name is "01_marketsplit",
+    # 2. its model_names is ["binary_linear", "binary_unconstrained"],
+    # 3. its availabe_instances is dict whose key are "binary_linear" and "binary_unconstrained",
+    # 4. each value of its availabe_instances is a list of str,
+    # 5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    # 6. the evaluated solution with its instance and solution is the same as the original solution.
+
+
+def test_labs():
+    """Create a Labs instance and get each instances in its avialable_instances.
+
+    Check if
+    1. its name is "02_labs",
+    2. its model_names is ["integer", "quadratic_unconstrained"],
+    3. its availabe_instances is dict whose key are "integer" and "quadratic_unconstrained",
+    4. each value of its availabe_instances is a list of str,
+    5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    6. the evaluated solution with its instance and solution is the same as the original solution.
+    """
+    # 1. its name is "02_labs",
+    # 2. its model_names is ["integer", "quadratic_unconstrained"],
+    # 3. its availabe_instances is dict whose key are "integer" and "quadratic_unconstrained",
+    # 4. each value of its availabe_instances is a list of str,
+    # 5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    # 6. the evaluated solution with its instance and solution is the same as the original solution.
+
+
+def test_birkhoff():
+    """Create a Birkhoff instance and get each instances in its avialable_instances.
+
+    Check if
+    1. its name is "03_birkhoff",
+    2. its model_names is ["integer_linear"],
+    3. its availabe_instances is dict whose key are "integer_linear",
+    4. each value of its availabe_instances is a list of str,
+    5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    6. the evaluated solution with its instance and solution is the same as the original solution.
+    """
+    # 1. its name is "03_birkhoff",
+    # 2. its model_names is ["integer_linear"],
+    # 3. its availabe_instances is dict whose key are "integer_linear",
+    # 4. each value of its availabe_instances is a list of str,
+    # 5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    # 6. the evaluated solution with its instance and solution is the same as the original solution.
+
+
+def test_steiner():
+    """Create a Steiner instance and get each instances in its avialable_instances.
+
+    Check if
+    1. its name is "04_steiner",
+    2. its model_names is ["integer_linear"],
+    3. its availabe_instances is dict whose key are "integer_linear",
+    4. each value of its availabe_instances is a list of str,
+    5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    6. the evaluated solution with its instance and solution is the same as the original solution.
+    """
+    # 1. its name is "04_steiner",
+    # 2. its model_names is ["integer_linear"],
+    # 3. its availabe_instances is dict whose key are "integer_linear",
+    # 4. each value of its availabe_instances is a list of str,
+    # 5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    # 6. the evaluated solution with its instance and solution is the same as the original solution.
+
+
+def test_sports():
+    """Create a Sports instance and get each instances in its avialable_instances.
+
+    Check if
+    1. its name is "05_sports",
+    2. its model_names is ["mixed_integer_linear"],
+    3. its availabe_instances is dict whose key are "mixed_integer_linear",
+    4. each value of its availabe_instances is a list of str,
+    5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    6. the evaluated solution with its instance and solution is the same as the original solution.
+    """
+    # 1. its name is "05_sports",
+    # 2. its model_names is ["mixed_integer_linear"],
+    # 3. its availabe_instances is dict whose key are "mixed_integer_linear",
+    # 4. each value of its availabe_instances is a list of str,
+    # 5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    # 6. the evaluated solution with its instance and solution is the same as the original solution.
+
+
+def test_portfolio():
+    """Create a Portfolio instance and get each instances in its avialable_instances.
+
+    Check if
+    1. its name is "06_portfolio",
+    2. its model_names is ["binary_quadratic", "quadratic_unconstrained"],
+    3. its availabe_instances is dict whose key are "binary_quadratic" and "quadratic_unconstrained",
+    4. each value of its availabe_instances is a list of str,
+    5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    6. the evaluated solution with its instance and solution is the same as the original solution.
+    """
+    # 1. its name is "06_portfolio",
+    # 2. its model_names is ["binary_quadratic", "quadratic_unconstrained"],
+    # 3. its availabe_instances is dict whose key are "binary_quadratic" and "quadratic_unconstrained",
+    # 4. each value of its availabe_instances is a list of str,
+    # 5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    # 6. the evaluated solution with its instance and solution is the same as the original solution.
+
+
+def test_independent_set():
+    """Create a IndependentSet instance and get each instances in its avialable_instances.
+
+    Check if
+    1. its name is "07_independent_set",
+    2. its model_names is ["binary_linear", "binary_unconstrained"],
+    3. its availabe_instances is dict whose key are "binary_linear" and "binary_unconstrained",
+    4. each value of its availabe_instances is a list of str,
+    5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    6. the evaluated solution with its instance and solution is the same as the original solution.
+    """
+    # 1. its name is "07_independent_set",
+    # 2. its model_names is ["binary_linear", "binary_unconstrained"],
+    # 3. its availabe_instances is dict whose key are "binary_linear" and "binary_unconstrained",
+    # 4. each value of its availabe_instances is a list of str,
+    # 5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    # 6. the evaluated solution with its instance and solution is the same as the original solution.
+
+
+def test_network():
+    """Create a Network instance and get each instances in its avialable_instances.
+
+    Check if
+    1. its name is "08_network",
+    2. its model_names is ["integer_linear"],
+    3. its availabe_instances is dict whose key are "integer_linear",
+    4. each value of its availabe_instances is a list of str,
+    5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    6. the evaluated solution with its instance and solution is the same as the original solution.
+    """
+    # 1. its name is "08_network",
+    # 2. its model_names is ["integer_linear"],
+    # 3. its availabe_instances is dict whose key are "integer_linear",
+    # 4. each value of its availabe_instances is a list of str,
+    # 5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    # 6. the evaluated solution with its instance and solution is the same as the original solution.
+
+
+def test_routing():
+    """Create a Routing instance and get each instances in its avialable_instances.
+
+    Check if
+    1. its name is "09_routing",
+    2. its model_names is ["integer_linear"],
+    3. its availabe_instances is dict whose key are "integer_linear",
+    4. each value of its availabe_instances is a list of str,
+    5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    6. the evaluated solution with its instance and solution is the same as the original solution.
+    """
+    # 1. its name is "09_routing",
+    # 2. its model_names is ["integer_linear"],
+    # 3. its availabe_instances is dict whose key are "integer_linear",
+    # 4. each value of its availabe_instances is a list of str,
+    # 5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    # 6. the evaluated solution with its instance and solution is the same as the original solution.
+
+
+def test_topology():
+    """Create a Topology instance and get each instances in its avialable_instances.
+
+    Check if
+    1. its name is "10_topology",
+    2. its model_names is ["flow_mip", "seidel_linear", "seidel_quadratic"],
+    3. its availabe_instances is dict whose key are "flow_mip", "seidel_linear" and "seidel_quadratic",
+    4. each value of its availabe_instances is a list of str,
+    5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    6. the evaluated solution with its instance and solution is the same as the original solution.
+    """
+    # 1. its name is "10_topology",
+    # 2. its model_names is ["flow_mip", "seidel_linear", "seidel_quadratic"],
+    # 3. its availabe_instances is dict whose key are "flow_mip", "seidel_linear" and "seidel_quadratic",
+    # 4. each value of its availabe_instances is a list of str,
+    # 5. the returned value of __call__ is a tuple of (ommx.v1.instance, ommx.v1.solution) using each values of its availabe_instances,
+    # 6. the evaluated solution with its instance and solution is the same as the original solution.
